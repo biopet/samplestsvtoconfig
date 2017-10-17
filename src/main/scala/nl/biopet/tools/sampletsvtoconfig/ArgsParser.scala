4b1871c23968db0850cba8489d36ddfcf7ef5ac0
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-10-17 16:50:59 +0200

    review

--- src/main/scala/nl/biopet/tools/sampletsvtoconfig/ArgsParser.scala
+++ src/main/scala/nl/biopet/tools/sampletsvtoconfig/ArgsParser.scala
@@ -8,7 +8,7 @@ class ArgsParser(cmdName: String) extends AbstractOptParser[Args](cmdName) {
   opt[File]('i', "inputFiles") unbounded () valueName "<file>" action {
     (x, c) =>
       c.copy(inputFiles = x :: c.inputFiles)
-  } text "Input must be a tsv file, first line is seen as header and must at least have a 'sample' column, 'library' column is optional, multiple files allowed" // What is the syntax for adding multiple files. You have to specify the flag only once or multiple times?
+  } text "Input must be a tsv file, first line is seen as header and must at least have a 'sample' column, 'library' column is optional, multiple files can be specified by using multiple flags."
   opt[File]('t', "tagFiles") unbounded () valueName "<file>" action { (x, c) =>
     c.copy(tagFiles = x :: c.tagFiles)
   }
@@ -17,6 +17,5 @@ class ArgsParser(cmdName: String) extends AbstractOptParser[Args](cmdName) {
       c.copy(outputFile = Some(x))
   } text """
            |When the extension is .yml or .yaml the output is in yaml format, otherwise it is in json.
-           |When no extension is given the output goes to stdout as yaml.
-         """.stripMargin // This is unclear. An outputfile is referenced but stdout is mentioned as well? So the output in stdout differs from that of the file?
-}
+           |When no file is given the output goes to stdout as yaml.
+         """.stripMargin
